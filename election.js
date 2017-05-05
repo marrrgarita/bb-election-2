01b3bd68f955753b0f9fceeea2af670db21b966b
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-04 21:58:12 -0400

    make voting buttons work asynchronously

--- election.js
+++ election.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
   // Imagination!
+  // var voteURL = 'https://bb-election-api.herokuapp.com/vote'
+
 
   $.ajax({
     url: 'https://bb-election-api.herokuapp.com/',
@@ -11,12 +13,29 @@ $(document).ready(function() {
     for (var i = 0; i < data.candidates.length; i++) {
       candidate = data.candidates[i];
       var cInfo = candidate.name + ': ' + candidate.votes + ' votes.';
-      var item = $('<li>').html(cInfo);
+      var voteForm = '<form class="vote-form" method="post" action="https://bb-election-api.herokuapp.com/vote"><input type="hidden" name="name" value="' + candidate.name + '"><button type="submit">VOTE</button></form>';
+      // var voteInput = '<input type="hidden" name="name" value="Gary">'
+      // var submitButton = '<button type="submit">Submit</button>'
+
+      var item = $('<li>').html(cInfo + voteForm);
       $('#candidate-list').append(item);
+
     }
+  });
 
-
-
+  $('body').on('submit', '.vote-form', function(e){
+    e.preventDefault();
+    var that = $(this).children('input[type=hidden]').val();
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: { name: $(this).children('input[type=hidden]').val()},
+      dataType: 'json'
+    }).done(function(data){
+      console.log(data);
+    }).always(function(){
+      console.log(that);
+    });
   });
 
 });
